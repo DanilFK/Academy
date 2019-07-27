041e3bfba5d5c7a0b41886190002a85bd0c1e856
DK <danilbelov44ru@gmail.com>
2019-07-27 22:59:09 +0400

    LearnJS

--- Academy/JS.js
+++ Academy/JS.js
@@ -1,1 +1,18 @@
 "use strict";
+
+var admin;
+var name = "Vasiliy";
+admin = name;
+console.log(admin);
+var planet = "Earth";
+var user = "Petya";
+console.log(user + " from " + planet);
+
+function isInteger(num){
+    if(num == (num^0)){
+        return true;
+    }
+    else{return false;}
+}
+console.log(isInteger(5));
+console.log(isInteger(1.5));
