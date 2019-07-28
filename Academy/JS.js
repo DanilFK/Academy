6be0807230eba0f22b1ab290de1380b85444d11f
DK <danilbelov44ru@gmail.com>
2019-07-28 23:32:47 +0400

    logic operations

--- Academy/JS.js
+++ Academy/JS.js
@@ -4,5 +4,5 @@ var age = +prompt("Write your age: ", "");
 if(age >= 14 && age <= 90){
     alert("Yeah, cool"); 
 }
-else if(age < 14 || age > 90){alert("Babies have no access");}
+else if(age < 14 || age > 90    ){alert("Babies have no access");}
 else{alert("either you're too old or you're too stupid and have inputed wrong age");}
